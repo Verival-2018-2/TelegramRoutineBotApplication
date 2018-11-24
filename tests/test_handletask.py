cc38be993063faf253e59995539e2d4b934fc60d
João Vitor <joaovytor0@gmail.com>
2018-11-24 18:51:33 -0200

    Adiciona novos testes e retira funções não utilizadas

--- tests/test_handletask.py
+++ tests/test_handletask.py
@@ -1,8 +1,18 @@
 import sys
 sys.path.insert(0, '../myroutinebot/')
 from myroutinebot.handletask import HandleTask 
+from myroutinebot.bot import Bot
 import pytest
+
 
 def test_task_condition():
     handletask = HandleTask()
     assert handletask.task_condition('teste', 'teste') == True
+
+def test_check_msg_not_exists():
+    handletask = HandleTask()
+    assert handletask.check_msg_not_exists('test') == True
+
+def test_condition_len_msg():
+    handletask = HandleTask()
+    assert handletask.condition_len_msg('2, test') == True
